b1f077d45efc6a708b20737364193e5d9a5aebf2
Hippolyte Bach <hbach@juniorisep.com>
2022-11-22 22:26:10 +0100

    disable cach for dev

--- src/index.php
+++ src/index.php
@@ -1,4 +1,8 @@
 <?php
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
 
 if(isset($_GET['url']) && !empty($_GET['url'])) {
     $url = str_replace([".html", ".php"], "",$_GET['url']);
