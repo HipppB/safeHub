01356321a0cd0989d406a1a23b2aad47ba97c79f
overwatch <ahenrion@juniorisep.com>
2022-12-08 01:42:59 +0100

    update docker

--- src/index.php
+++ src/index.php
@@ -1,12 +1,12 @@
 <?php
 declare(strict_types=1);
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 session_start();
 header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/..');
 $dotenv->load();
 if (!isset($url)) {
     $url = 'index';
--- src/views/auth/dashboard.php
+++ src/views/auth/dashboard.php
@@ -1,53 +1,52 @@
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width" />
-        <title>Dashboard</title>
-        <link rel="stylesheet" href="../views/styles/headerPrivate.css" />
-        <link rel="stylesheet" href="../views/styles/dashboard.css" />
-        <link rel="stylesheet" href="../views/styles/common/index.css" />
-        
-        <script
-            type="text/javascript"
-            src="../views/scripts/common/components.js"
-            async
-        ></script>
-    </head>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../views/styles/headerPrivate.css" />
+    <link rel="stylesheet" href="../views/styles/dashboard.css" />
+    <link rel="stylesheet" href="../views/styles/common/index.css" />
 
-    <body>
+    <script
+        type="text/javascript"
+        src="../views/scripts/common/components.js"
+        async
+    ></script>
+</head>
+
+<body>
+<div
+    class="header-container"
+    title="Dashboard"
+    leftButtonPath="../views/assets/icons/backButton.svg"
+    rightAction="window.location.href = './profile';"
+
+></div>
+
+<main>
+    <div id="text">
+        <p>Vous n'avez pas de produit associé à votre compte</p>
+        <hr />
+        <p>
+            Rentrez le code produit qui vous a été donné afin de les
+            importer.
+        </p>
+    </div>
+    <form method="POST" id='form'>
         <div
-            class="header-container"
-            title="Dashboard"
-            leftButtonPath="../views/assets/icons/backButton.svg"
-            rightAction="window.location.href = './profile';"
-
+            class="input-label-container"
+            name="productUserCode"
+            placeholder="Code produit"
         ></div>
-
-        <main>
-            <div id="text">
-                <p>Vous n'avez pas de produit associé à votre compte</p>
-                <hr />
-                <p>
-                    Rentrez le code produit qui vous a été donné afin de les
-                    importer.
-                </p>
-            </div>
-            <form method="POST" id='form'>
-                <div
-                    class="input-label-container"
-                    name="productUserCode"
-                    placeholder="Code produit"
-                ></div>
-                <?php if ($error == 401) {
-                    echo "<p class='error'>Code produit incorrect</p>";
-                } ?>
-            <input type="submit" class="button mT25" value="Ajouter" />
-            </form>
-        </main>
-        <!-- Footer -->
-        <div class="footer-container" small="true"></div>
-    </body>
+        <?php if ($error == 401) {
+            echo "<p class='error'>Code produit incorrect</p>";
+        } ?>
+        <input type="submit" class="button mT25" value="Ajouter" />
+    </form>
+</main>
+<!-- Footer -->
+<div class="footer-container" small="true"></div>
+</body>
 </html>
 
