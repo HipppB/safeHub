9ff940bb10bf20702cc38d989036437115e9e13b
overwatch <ahenrion@juniorisep.com>
2022-12-09 14:29:02 +0100

    allow $_ENV variable refactoring connect db

--- src/model/connectDb.php
+++ src/model/connectDb.php
@@ -3,9 +3,9 @@
 function connectDb() {
     try {
 
-         $db = new PDO('mysql:host=mysql;dbname=SafeHub;charset=utf8', 'admin', 'password', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+//         $db = new PDO('mysql:host=mysql;dbname=SafeHub;charset=utf8', 'admin', 'password', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-//        $db = new PDO("mysql:host=".$_ENV["DATABASE_HOST"].";dbname=".$_ENV["DATABASE_NAME"].";charset=utf8", $_ENV["DATABASE_USER"], $_ENV["DATABASE_PASSWORD"]);
+        $db = new PDO("mysql:host=".$_ENV["MYSQL_HOST"].";dbname=".$_ENV["MYSQL_DATABASE"].";charset=utf8", $_ENV["MYSQL_USER"], $_ENV["MYSQL_PASSWORD"]);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     } catch (PDOException $e) {
