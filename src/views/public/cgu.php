3574eb511331ea198f732b302c9e3f4b56f917cb
Clashix <84671657+Clashix@users.noreply.github.com>
2022-12-12 10:35:11 +0100

    cgu

--- src/views/public/cgu.php
+++ src/views/public/cgu.php
@@ -18,24 +18,22 @@
     <?php require 'views/components/header.php'; ?>
         <div class="mL20">
             <p class="gradienttext title mT50">CGU</p>
-            <p class="updateTitle">Dernière mise à jour le X</p>
+            <p class="updateTitle">Dernière mise à jour le 12/12/2022</p>
         </div>
         <div class="line mT50"></div>
         <div class="mL20 mR20 mT25 mB50">
             <div class="mT20">
-                <p class="cguTitle">1. Lorem</p>
+                <p class="cguTitle">1. Conditions générales d'utilisation</p>
                 <p class="paragraphCgu mT10">
-                <h1>Conditions générales d'utilisation</h1>
-                <p>En utilisant notre site web et notre produit SafeHub, vous acceptez les conditions générales d'utilisation suivantes :</p>
-                <ul>
-                <li>Le produit SafeHub est destiné à être utilisé uniquement dans un appartement pour surveiller la fréquence cardiaque, la température et le niveau sonore des locataires et du propriétaire.</li>
-                <li>Nous ne sommes pas responsables des dommages causés par l'utilisation incorrecte du produit SafeHub.</li>
-                <li>Nous nous réservons le droit de modifier ces conditions générales d'utilisation à tout moment, en publiant les nouvelles conditions sur notre site web.</li>
-                <li>En utilisant notre site web et notre produit SafeHub, vous acceptez de recevoir des communications marketing de notre part.</li>
-                </ul>
+                En utilisant notre site web et notre produit SafeHub, vous acceptez les conditions générales d'utilisation suivantes :      
+                <br/>- Le produit SafeHub est destiné à être utilisé uniquement dans un appartement pour surveiller la fréquence cardiaque, la température et le niveau sonore des locataires et du propriétaire.
+                <br/>- Nous ne sommes pas responsables des dommages causés par l'utilisation incorrecte du produit SafeHub.
+                <br/>- Nous nous réservons le droit de modifier ces conditions générales d'utilisation à tout moment, en publiant les nouvelles conditions sur notre site web.
+                <br/>- En utilisant notre site web et notre produit SafeHub, vous acceptez de recevoir des communications marketing de notre part.
+                
                 </p>
             </div>
-            <div class="mT20">
+            <!-- <div class="mT20">
                 <p class="cguTitle">2. Lorem</p>
                 <p class="paragraphCgu mT10">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
@@ -66,7 +64,7 @@
                     volutpat. Diam donec adipiscing tristique risus nec feugiat
                     in fermentum.
                 </p>
-            </div>
+            </div> -->
         </div>
         <!-- Footer -->
         <div class="footer-container" small="false"></div>
