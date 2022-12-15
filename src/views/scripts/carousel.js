a9f863f3a6521f9362b93f6e23abadc4d450b7dd
Hippolyte Bach <hbach@juniorisep.com>
2022-12-16 00:59:38 +0100

    fix

--- src/views/scripts/carousel.js
+++ src/views/scripts/carousel.js
@@ -18,19 +18,20 @@ function currentSlide(n) {
 function showSlides(n) {
     let i
     let slides = document.getElementsByClassName('slides')
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none'
-    }
+    slides[0].style.display = 'block'
+    // if (n > slides.length) {
+    //     slideIndex = 1
+    // }
+    // if (n < 1) {
+    //     slideIndex = slides.length
+    // }
+    // for (i = 0; i < slides.length; i++) {
+    //     slides[i].style.display = 'none'
+    // }
 
-    if (slides.length > slideIndex) {
-        slides[slideIndex - 1].style.display = 'block'
-    }
+    // if (slides.length > slideIndex) {
+    //     slides[slideIndex - 1].style.display = 'block'
+    // }
 }
 
 // for (let i = 0; i < button.length; i++) {
