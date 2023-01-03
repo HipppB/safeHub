0b3e782b723951eeeb93d54258124827fe251242
Clashix <84671657+Clashix@users.noreply.github.com>
2023-01-03 15:03:38 +0100

    first

--- src/model/translate.requests.php
+++ src/model/translate.requests.php
@@ -6,23 +6,22 @@ function translate($key, $lang)
 {
     global $db;
     try {
-    $query = $db->prepare(
-        "SELECT * 
+        $query = $db->prepare(
+            "SELECT * 
         FROM translations
         WHERE `key` = :keyrequested AND `lang` = :langrequested
         "
-    );
-    //SELECT * FROM translations WHERE `key` = 'home_subtitle' AND `lang` = 'fr'
-    $query->execute([
-        'keyrequested' => $key,
-        'langrequested' => $lang,
-    ]);
-    return $query->fetch()['value'];
-
-} catch (PDOException $e) {
-    echo $e;
-    return 'Undefined';
-}
+        );
+        //SELECT * FROM translations WHERE `key` = 'home_subtitle' AND `lang` = 'fr'
+        $query->execute([
+            'keyrequested' => $key,
+            'langrequested' => $lang,
+        ]);
+        return $query->fetch()['value'];
+    } catch (PDOException $e) {
+        echo $e;
+        return 'Undefined';
+    }
 }
 
 function printTranslation($key, $noprint = false)
@@ -33,4 +32,20 @@ function printTranslation($key, $noprint = false)
         echo $result;
     }
     return $result;
+}
+
+function updateTranslation($key, $value, $lang)
+{
+    global $db;
+    $query = $db->prepare(
+        'UPDATE translations SET value = :value WHERE id = :id, lang = :lang'
+    );
+    try {
+        $query->execute([
+            'value' => $value,
+        ]);
+    } catch (PDOException $error) {
+        echo $error;
+        return 'Undefined';
+    }
 }
