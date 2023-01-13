65c5b23f0588577abbf485773506a93846edd5ff
Hippolyte Bach <hbach@juniorisep.com>
2023-01-13 23:24:04 +0100

    fix footer design

--- src/views/components/footer.php
+++ src/views/components/footer.php
@@ -1,11 +1,11 @@
 <div class="footer-container">
-    <p class="gradienttext mT10">Crée par AllSafe - Copyright 2022</p>
     <?php if (strpos($_SERVER['REQUEST_URI'], 'panel') == false) {
-        echo '<div class="line mT25"></div>';
-
         echo '<a href="../cgu">Conditions générales \'utilisation</a>
             <a href="../mentionslegales">Mentions légales</a>
             <a href="../faq">FAQ</a>
             <a href="../contact">Contact</a>';
+        echo '<div class="line mT25"></div>';
     } ?>
+        <p class="gradienttext mT10">Crée par AllSafe - Copyright 2022</p>
+
 </div>
