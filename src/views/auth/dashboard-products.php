882f493f5d193d7b72f383a934fec881b2b4e201
overwatch <henrion.adeline@gmail.com>
2023-01-16 11:57:22 +0100

    add parameters to links

--- src/views/auth/dashboard-products.php
+++ src/views/auth/dashboard-products.php
@@ -24,44 +24,42 @@
 
     <main>
 
-      <?php foreach ($products as $product) {
-          echo "
+      <?php foreach ($products as $product) { ?>
           <section>
             <header>
               <img src='../views/assets/icons/homeInline.svg' alt='house'>
-              <h2 class='gradienttext'>" .
-              $product['product_name'] .
-              ' (' .
-              $product['room_name'] .
-              ")</h2>
+              <h2 class='gradienttext'>
+              <?php echo $product['product_name'] ?>
+              (
+              <?php echo $product['room_name']?>
+              )</h2>
             </header>
             <div class='graphList'>
-            <a href='./datas'>
+            <a href='./datas?productId=<?php echo $product['id']?>&type=temperature'>
             <div>
                <img src='../views/assets/graph.svg' alt='graph'/>
              Température</div>
 </a>
 
-                            <a href='./datas'>
+                            <a href='./datas?productId=<?php echo $product['id']?>&type=humidity'>
                             <div>
                         <img src='../views/assets/graph.svg' alt='graph'/>
                         Humidité</div></a>
-           
-                          <a href='./datas'>
+
+                          <a href='./datas?productId=<?php echo $product['id']?>&type=carbon_dioxide'>
                           <div>
               <img src='../views/assets/graph.svg' alt='graph'/>
 CO2</div>
                 </a>
 
-                          <a href='./datas'>
+                          <a href='./datas?productId=<?php echo $product['id']?>&type=sound_level'>
                           <div>
                <img src='../views/assets/graph.svg' alt='graph'/>
 Signal Sonore</div></a>
-             
+
             </div>
         </section>
-        ";
-      } ?>
+<?php      } ?>
     </main>
     <!-- Footer -->
     <div class="footer-container" small="true"></div>
