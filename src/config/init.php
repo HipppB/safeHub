1e6511f19cb680dcdfbe3be65a649cb477c5abb6
overwatch <henrion.adeline@gmail.com>
2023-01-17 10:44:47 +0100

    refactor json import to fix import bugs for both import solution

--- src/config/init.php
+++ src/config/init.php
@@ -1,12 +1,17 @@
 <?php
 require_once '../../vendor/autoload.php';
+require '../model/connectDb.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/../..');
 $dotenv->load();
 
-require '../model/connectDb.php';
 $db = connectDb();
+include 'dropDb.php';
 $sql = file_get_contents('mvc.sql');
 $db->exec($sql);
-
-require '../model/insertData.php';
+echo 'Database created' . PHP_EOL;
+$translations = json_decode(
+    file_get_contents('./translations.json'),
+    true
+);
+include '../model/insertData.php';
