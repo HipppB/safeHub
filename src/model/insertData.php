4de4c593403be3ad747499fa131de03ea0fb7f9b
overwatch <henrion.adeline@gmail.com>
2023-01-17 10:42:30 +0100

    refactor deleting datas to drop table in another file, fix bugs

--- src/model/insertData.php
+++ src/model/insertData.php
@@ -1,40 +1,46 @@
 <?php
-//connect to db and insert fake data
-require_once 'connectDb.php'; // Already declared at source in translation.requests.php
-echo 'connectDb required' . '<br>';
-$db = connectDb();
+// insertData.php
+
+/** @var PDO $db */
+/**    @var array $translations
+ */
 //deletes existing users and insert fake users
+
+// Not useful if you use the dropDb.php script
+//$queryDeleteAllUser = $db->prepare('DELETE FROM users');
+//$queryDeleteAllProducts = $db->prepare('DELETE FROM products');
+//$queryDeleteAllProducts->execute();
+//$queryDeleteAllUser->execute();
+
 $queryAddUser = $db->prepare("INSERT INTO users 
     (`name`, `lastname`, `password`, `email`, `phone`, `birth_date`, `is_admin`) 
     VALUES (:name, :lastname, :password, :email, :phone, :birth_date, :is_admin)");
-echo 'request prepared' . '<br>';
 
-$queryDeleteAllUser = $db->prepare('DELETE FROM users');
-echo 'request prepared' . '<br>';
-$queryDeleteAllProducts = $db->prepare('DELETE FROM products');
-$queryDeleteAllProducts->execute();
-$queryDeleteAllUser->execute();
-$queryAddUser->execute([
-    'email' => 'admin@test.com',
-    'password' => password_hash($_ENV['PASS'], PASSWORD_DEFAULT),
-    'name' => 'admin',
-    'lastname' => 'admin',
-    'phone' => null,
-    'birth_date' => '2001-12-06',
-    'is_admin' => '1',
-]);
+try {
+    $queryAddUser->execute([
+        'email' => 'admin@test.com',
+        'password' => password_hash($_ENV['PASS'], PASSWORD_DEFAULT),
+        'name' => 'admin',
+        'lastname' => 'admin',
+        'phone' => null,
+        'birth_date' => '2001-12-06',
+        'is_admin' => '1',
+    ]);
+    $queryAddUser->execute([
+        'email' => 'user@test.com',
+        'password' => password_hash($_ENV['PASS'], PASSWORD_DEFAULT),
+        'name' => 'user',
+        'lastname' => 'user',
+        'phone' => '+33 6 78 90 12 34',
+        'birth_date' => '2002-12-06',
+        'is_admin' => '0',
+    ]);
+    echo 'Fake users inserted' . PHP_EOL;
+}catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage() . PHP_EOL;
+}
 
-$queryAddUser->execute([
-    'email' => 'user@test.com',
-    'password' => password_hash($_ENV['PASS'], PASSWORD_DEFAULT),
-    'name' => 'user',
-    'lastname' => 'user',
-    'phone' => '+33 6 78 90 12 34',
-    'birth_date' => '2002-12-06',
-    'is_admin' => '0',
-]);
 
-echo 'Fake users inserted' . '<br>';
 
 // create fake products
 
@@ -73,10 +79,10 @@ try {
         'db_max' => '0',
         'temp_max' => '0',
     ]);
+    echo 'Fake products created' . PHP_EOL;
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo $e->getMessage() . PHP_EOL;
 }
-echo 'Fake products created';
 
 // roles
 $queryAddRoles = $db->prepare("INSERT INTO roles
@@ -87,51 +93,49 @@ try {
         'id' => '1',
         'name' => 'admin',
     ]);
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-
-try {
     $queryAddRoles->execute([
         'id' => '2',
         'name' => 'user',
     ]);
+    echo 'Fake roles created' . PHP_EOL;
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo $e->getMessage() . PHP_EOL;
 }
 
 // Import translations from json file
-$translations = json_decode(
-    file_get_contents('config/translations.json'),
-    true
-);
+
 $queryAddTranslation = $db->prepare("INSERT INTO translations
     (`key`, `lang`, `value`) 
     VALUES (:key, :lang, :value)");
-$queryDeleteAllTranslations = $db->prepare('DELETE FROM translations');
-$queryDeleteAllTranslations->execute();
-foreach ($translations as $key => $value) {
-    foreach ($value as $lang => $translation) {
-        $queryAddTranslation->execute([
-            'key' => $key,
-            'lang' => $lang,
-            'value' => $translation,
-        ]);
+
+try {
+    foreach ($translations as $key => $value) {
+        foreach ($value as $lang => $translation) {
+            $queryAddTranslation->execute([
+                'key' => $key,
+                'lang' => $lang,
+                'value' => $translation,
+            ]);
+        }
     }
+    echo 'Fake translations created' . PHP_EOL;
+}catch (PDOException $e) {
+    echo $e->getMessage() . PHP_EOL;
 }
-$translations = json_decode(file_get_contents('translations.json'), true);
-$queryAddTranslation = $db->prepare("INSERT INTO translations
-    (`key`, `lang`, `value`) 
-    VALUES (:key, :lang, :value)");
-foreach ($translations as $key => $value) {
-    foreach ($value as $lang => $translation) {
-        $queryAddTranslation->execute([
-            'key' => $key,
-            'lang' => $lang,
-            'value' => $translation,
-        ]);
-    }
-}
+// not useful ???
+//$translations = json_decode(file_get_contents('translations.json'), true);
+//$queryAddTranslation = $db->prepare("INSERT INTO translations
+//    (`key`, `lang`, `value`)
+//    VALUES (:key, :lang, :value)");
+//foreach ($translations as $key => $value) {
+//    foreach ($value as $lang => $translation) {
+//        $queryAddTranslation->execute([
+//            'key' => $key,
+//            'lang' => $lang,
+//            'value' => $translation,
+//        ]);
+//    }
+//}
 
 $queryAddTips = $db->prepare("INSERT INTO tips
     (`id`, `content`) 
@@ -143,72 +147,59 @@ try {
         'content' =>
             'Ceci est un test de conseil tres tres tres tres long parce que je veux tester le css',
     ]);
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-
-try {
     $queryAddTips->execute([
         'id' => '4',
         'content' =>
             'Ceci est un test de conseil tres tres tres tres long parce que je veux tester le css',
     ]);
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-
-try {
     $queryAddTips->execute([
         'id' => '5',
         'content' =>
             'Ceci est un test de conseil tres tres tres tres long parce que je veux tester le css',
     ]);
+    echo 'Fake tips created' . PHP_EOL;
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo $e->getMessage() . PHP_EOL;
 }
-
-echo 'Fake translations created';
-
 // insert fake data types and fake datas
 
 $queryAddMetricsTypes = $db->prepare("INSERT INTO types
-    (`id`, `type_name`) 
-    VALUES (:id, :name)");
+    (`type_name`) 
+    VALUES (:name)");
 
 try {
     $queryAddMetricsTypes->execute([
-        'id' => '1',
         'name' => 'temperature',
     ]);
     $queryAddMetricsTypes->execute([
-        'id' => '2',
         'name' => 'humidity',
     ]);
     $queryAddMetricsTypes->execute([
-        'id' => '3',
         'name' => 'carbon_dioxide',
     ]);
     $queryAddMetricsTypes->execute([
-        'id' => '4',
         'name' => 'sound_level',
     ]);
 
-    echo 'Fake data types created';
+    echo 'Fake data types created' . PHP_EOL;
 
 
 
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo $e->getMessage() . PHP_EOL;
 }
 
 
 $queryAddMetrics = $db->prepare("INSERT INTO metrics (`id`, `id_product`, `id_type`, `data`, `date`) VALUES (:id, :id_product, :type_id, :value, :date)");
 $getProducts = $db->prepare('SELECT * FROM products');
+$getTypes = $db->prepare('SELECT * FROM types');
 
 try {
     $getProducts->execute();
     $products = $getProducts->fetchAll();
-    $i = 0;
+    $getTypes->execute();
+    $types = $getTypes->fetchAll();
+    $i = 1;
     while ($i < 100) {
         $timestamp = strtotime("2023-01-14 12:47:14")+(60*60*$i);
         $date = new DateTime();
@@ -216,35 +207,35 @@ try {
         $queryAddMetrics->execute([
             'id' => $i,
             'id_product' => $products[0]['id'],
-            'type_id' => '1',
+            'type_id' => $types[0]['id'],
             'value' => rand(0, 100),
             'date' => $date->format('Y-m-d H:i:s'),
         ]);
         $queryAddMetrics->execute([
             'id' => $i + 1,
             'id_product' => $products[0]['id'],
-            'type_id' => '2',
+            'type_id' => $types[1]['id'],
             'value' => rand(0, 100),
             'date' => $date->format('Y-m-d H:i:s'),
         ]);
         $queryAddMetrics->execute([
             'id' => $i + 2,
             'id_product' => $products[0]['id'],
-            'type_id' => '3',
+            'type_id' => $types[2]['id'],
             'value' => rand(0, 100),
             'date' => $date->format('Y-m-d H:i:s'),
         ]);
         $queryAddMetrics->execute([
             'id' => $i + 3,
             'id_product' => $products[0]['id'],
-            'type_id' => '4',
+            'type_id' => $types[3]['id'],
             'value' => rand(0, 100),
             'date' => $date->format('Y-m-d H:i:s'),
         ]);
         $i += 4;
     }
+    echo 'Fake metrics created' . PHP_EOL;
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo $e->getMessage()   . PHP_EOL;
 }
 
-echo 'Fake metrics created';
