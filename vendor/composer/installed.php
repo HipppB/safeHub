e3c273d61971b2e6a899da9b3c2989cb47e8380a
Clashix <84671657+Clashix@users.noreply.github.com>
2023-01-20 11:43:00 +0100

    mentions

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,86 +1,86 @@
-<?php return array(
-    'root' => array(
+<?php return [
+    'root' => [
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c584a542f4258bcb9a0d162b7aa5caab6c0f9c29',
+        'reference' => '6b8fd313ea40536b0f8a548eaee2f5af61fba60f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
+        'aliases' => [],
         'dev' => true,
-    ),
-    'versions' => array(
-        '__root__' => array(
+    ],
+    'versions' => [
+        '__root__' => [
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c584a542f4258bcb9a0d162b7aa5caab6c0f9c29',
+            'reference' => '6b8fd313ea40536b0f8a548eaee2f5af61fba60f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
+            'aliases' => [],
             'dev_requirement' => false,
-        ),
-        'graham-campbell/result-type' => array(
+        ],
+        'graham-campbell/result-type' => [
             'pretty_version' => 'v1.1.0',
             'version' => '1.1.0.0',
             'reference' => 'a878d45c1914464426dc94da61c9e1d36ae262a8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../graham-campbell/result-type',
-            'aliases' => array(),
+            'aliases' => [],
             'dev_requirement' => false,
-        ),
-        'phpmailer/phpmailer' => array(
+        ],
+        'phpmailer/phpmailer' => [
             'pretty_version' => 'v6.7.1',
             'version' => '6.7.1.0',
             'reference' => '49cd7ea3d2563f028d7811f06864a53b1f15ff55',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpmailer/phpmailer',
-            'aliases' => array(),
+            'aliases' => [],
             'dev_requirement' => false,
-        ),
-        'phpoption/phpoption' => array(
+        ],
+        'phpoption/phpoption' => [
             'pretty_version' => '1.9.0',
             'version' => '1.9.0.0',
             'reference' => 'dc5ff11e274a90cc1c743f66c9ad700ce50db9ab',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpoption/phpoption',
-            'aliases' => array(),
+            'aliases' => [],
             'dev_requirement' => false,
-        ),
-        'symfony/polyfill-ctype' => array(
+        ],
+        'symfony/polyfill-ctype' => [
             'pretty_version' => 'v1.27.0',
             'version' => '1.27.0.0',
             'reference' => '5bbc823adecdae860bb64756d639ecfec17b050a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../symfony/polyfill-ctype',
-            'aliases' => array(),
+            'aliases' => [],
             'dev_requirement' => false,
-        ),
-        'symfony/polyfill-mbstring' => array(
+        ],
+        'symfony/polyfill-mbstring' => [
             'pretty_version' => 'v1.27.0',
             'version' => '1.27.0.0',
             'reference' => '8ad114f6b39e2c98a8b0e3bd907732c207c2b534',
             'type' => 'library',
             'install_path' => __DIR__ . '/../symfony/polyfill-mbstring',
-            'aliases' => array(),
+            'aliases' => [],
             'dev_requirement' => false,
-        ),
-        'symfony/polyfill-php80' => array(
+        ],
+        'symfony/polyfill-php80' => [
             'pretty_version' => 'v1.27.0',
             'version' => '1.27.0.0',
             'reference' => '7a6ff3f1959bb01aefccb463a0f2cd3d3d2fd936',
             'type' => 'library',
             'install_path' => __DIR__ . '/../symfony/polyfill-php80',
-            'aliases' => array(),
+            'aliases' => [],
             'dev_requirement' => false,
-        ),
-        'vlucas/phpdotenv' => array(
+        ],
+        'vlucas/phpdotenv' => [
             'pretty_version' => 'v5.5.0',
             'version' => '5.5.0.0',
             'reference' => '1a7ea2afc49c3ee6d87061f5a233e3a035d0eae7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../vlucas/phpdotenv',
-            'aliases' => array(),
+            'aliases' => [],
             'dev_requirement' => false,
-        ),
-    ),
-);
+        ],
+    ],
+];
