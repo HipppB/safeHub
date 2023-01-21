deb7639b0de276b34983f786f8522d490c32c6ad
Hippolyte Bach <hbach@juniorisep.com>
2023-01-21 17:41:26 +0100

    new pages

--- src/controller/panel/useradmin.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'model/user.requests.php';
-if (!userIsConnected()) {
-    header('Location: /connexion');
-}
-include 'views/auth/utilisateur-admin.html';
--- src/views/auth/user.php
+++ src/views/auth/user.php
@@ -6,9 +6,11 @@
         <link rel="stylesheet" href="../views/styles/common/index.css" />
         <link rel="stylesheet" href="../views/styles/headerprivate.css" />
         <link rel="stylesheet" href="../views/styles/profil.css" />
-        <link rel="stylesheet" href="../views/styles/utilisateur-admin.css" />
+        <link rel="stylesheet" href="../views/styles/dashboardGestionnaire.css" />
+        <link rel="stylesheet" href="../views/styles/user.css" />
+
         <link rel="stylesheet" href="../views/styles/common/classStyles.css" />
-        <!-- <link rel="stylesheet" href="./styles/dashboardGestionnaire.css" /> -->
+       
         <script
             type="text/javascript"
             src="../views/scripts/common/components.js"
@@ -16,14 +18,10 @@
         ></script>
         <title>SafeHub - utilisateur</title>
     </head>
-    <body>
-        <div
-            class="header-container"
-            title="utilisateur"
-            leftButtonPath="../views/assets/icons/backButton.svg"
-            rightButtonPath="../views/assets/icons/logout.svg"
-            width="25px"
-        ></div>
+    <?php require 'views/components/headerPrivate.php'; ?>
+
+    <body class="mT50 center">
+
 
         <div class="central-container">
             <h2 class="gradienttext">Nom Prénom</h2>
@@ -31,37 +29,45 @@
             <div class="small-stroke"></div>
             <div>+33 9 75 43 78 07</div>
             <div>example@example.com</div>
+</div>
+        <div class="input-list-container rwidth">
+        <div class="low-title">Produits</div>
 
+            <?php if ($products == false) {
+                echo '<div class="center small-text">Aucun produit</div>';
+            } else {
+                foreach ($products as $product) {
+                    echo '
+                    <div class="item-in-list rwidth" onclick="window.location.href = \'./product?productid=' .
+                        $product['id'] .
+                        '\';">
+                        <div>
+                            <div class="image-notif-normale">
+                            </div>
+                        </div>
+                        
+                        <div class="name">
+                            <div class="gradienttext s030">' .
+                        $product['product_name'] .
+                        '</div>
+                            <div class="s025 leftAl">' .
+                        $product['room_name'] .
+                        '</div>
+                        </div>
+  
+                        <div>
+                        <div class="small-2 s025 mR10">' .
+                        $product['user_code'] .
+                        '
+                        </div>
+                        </div>      
+                    </div>
+                    <div class="line rwidth"></div>
+
+                        ';
+                }
+            } ?>
         </div>
-            <blockquote>produiTTt</blockquote>
-            <p class="status mB05"></p>
-        </div>
-
-      
-
-            <div class="notif">
-                <div class="top">
-                    <div class="img"></div>
-                    <p class="gradienttext">nom du produit</p>
-                    <div class="small">10/02/2022</div>
-            </div>
-
-            <div class="STR"></div>
-                <div class="notif">
-                    <div class="top">
-                        <div class="img"></div>
-                        <p class="gradienttext">nom du produit</p>
-                        <div class="small">10/02/2022</div>
-                </div>
-
-            <div class="STR"></div>
-                <div class="notif">
-                    <div class="top">
-                        <div class="img"></div>
-                        <p class="gradienttext">nom du produit</p>
-                        <div class="small">10/02/2022</div>
-                </div>
-
         <!-- <div class="notif"> 
         <div class="top">
                 <div class="notif-importante">
@@ -88,4 +94,6 @@
 
         <img src="../views/assets/message_icon.svg" class="messageIcon" />
     </body>
+    <?php require 'views/components/footer.php'; ?>
+
 </html>
