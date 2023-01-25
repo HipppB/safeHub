c652206f297345e88337633e117e1357b0929dd8
Hippolyte Bach <hbach@juniorisep.com>
2023-01-25 18:39:50 +0100

    components

--- src/views/auth/user.php
+++ src/views/auth/user.php
@@ -20,13 +20,10 @@ defer            async
     </head>
    
     <body class="mT50 center">
-    <?php
-    require 'views/components/headerPrivate.php';
-    require 'views/components/userProfile.php';
-    ?>
-
-
-
+         <?php
+         require 'views/components/headerPrivate.php';
+         require 'views/components/userProfile.php';
+         ?>
 
         <div class="input-list-container rwidth">
         <div class="low-title">Produits</div>
@@ -35,47 +32,11 @@ defer            async
                 echo '<div class="center small-text">Aucun produit</div>';
             } else {
                 foreach ($products as $product) {
-                    echo '
-                    <div class="item-in-list rwidth" onclick="window.location.href = \'./product?productid=' .
-                        $product['id'] .
-                        '\';">
-                        <div>
-                            <div class="image-notif-normale">
-                            </div>
-                        </div>
-                        
-                        <div class="name">
-                            <div class="gradienttext s030">' .
-                        $product['product_name'] .
-                        '</div>
-                            <div class="s025 leftAl">' .
-                        $product['room_name'] .
-                        '</div>
-                        </div>
-  
-                        <div>
-                        <div class="small-2 s025 mR10">' .
-                        $product['user_code'] .
-                        '
-                        </div>
-                        </div>      
-                    </div>
-                    <div class="line rwidth"></div>
-
-                        ';
+                    require 'views/components/productListingAdmin.php';
                 }
             } ?>
         </div>
-        <!-- <div class="notif"> 
-        <div class="top">
-                <div class="notif-importante">
-                    <blockquote><div class="gradienttext">nom du produit</div></blockquote>
-                    <div class="right">10/02/2022</div>
-            </div>
-        <div> -->
-
-            
-
+    
         <div class="modifierProduit">
             <bouton class="button">Associer un produit</bouton>
             <bouton class="button-outlined">Promouvoir gestionnaire</bouton>
