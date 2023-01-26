54172c5b90c14d0fa72cacf0ddbf3f55b09ac2a1
Overwatch <henrion.adeline@gmail.com>
2023-01-26 02:28:20 +0100

    delete var dumps, add logic things

--- src/controller/forgotPassword.php
+++ src/controller/forgotPassword.php
@@ -13,25 +13,17 @@ if(!empty($email)) {
         $expDate = date("Y-m-d H:i:s", $expFormat);
         $key = bin2hex(random_bytes(16));
         $response = addTemporaryToken($key, $email, $expDate);
+        $url = $_ENV['DOMAIN_NAME'];
+        $response = sendEmail($email, "
+        <h1>Vous avez demandé à réinitialiser votre mot de passe</h1>
+    <p>
+        Pour réinitialiser votre mot de passe, veuillez cliquer sur le lien ci-dessous:
+    </p>
+    <a href=\"$url/resetPassword?token=$key\">Réinitialiser mon mot de passe</a>
+    ", 'Reinitialisation de mot de passe');
+    } else {
+        $response = "L'utilisateur n'existe pas";
     }
-    $url = $_ENV['DOMAIN_NAME'];
-    $token = md5($email);
-//    $response = sendEmail($email, "
-//    <h1>Vous avez demandé à réinitialiser votre mot de passe</h1>
-//<p>
-//    Pour réinitialiser votre mot de passe, veuillez cliquer sur le lien ci-dessous:
-//</p>
-//<a href=\"$url/resetPassword?token=$token\">Réinitialiser mon mot de passe</a>
-//", 'Réinitialisation de mot de passe');
 }
 
 include 'views/public/forgotPassword.php';
-
-var_dump($user);
-$test = [
-    [
-        'id' => 1,
-        'email' => 'kjfkjd',
-    ]
-];
-var_dump($test);
